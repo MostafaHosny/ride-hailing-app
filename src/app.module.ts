fb265d175d8a0fd97dcf70b0216d6209cca4bed8
MostafaHosny <mostafa.hosny2015@hotmail.com>
2024-02-13 10:44:08 +0100

    clean up client dir

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { RidesModule } from './rides/rides.module';
-
-@Module({
-  imports: [RidesModule],
-  controllers: [AppController]
-})
-export class AppModule {}
